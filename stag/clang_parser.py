93a86d44d92193a45c3f37ababea003cdf6ad60c
Austin Bingham <austin.bingham@gmail.com>
2012-10-21 20:40:00 +0200

    initial stub version of clang parser.

---
+++ stag/clang_parser.py
@@ -0,0 +1,26 @@
+class Parser:
+    """A parser for C/C++ code which uses an external clang-based
+    tool.
+
+    """
+
+    def __init__(self):
+        self._fname = None
+
+    @property
+    def fname(self):
+        return self._fname
+
+    def set_file(self, fname):
+        self._fname = fname
+
+    def definitions(self):
+        assert self._fname is not None
+
+        result = subprocess.check_output(
+            ["echo", "someFunction 42"],
+            universal_newline=True)
+
+        for r in result.split('\n'):
+            name, lineno = r.split()
+            yield name, int(lineno)
