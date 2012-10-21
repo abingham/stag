cbd652ad940d1d2a3043b422dfddf53acd67b90b
Austin Bingham <austin.bingham@gmail.com>
2012-10-21 15:54:57 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'stag',
+    version = '0.1',
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = 'Austin Bingham',
+    author_email = 'austin.bingham@gmail.com',
+    description = 'A polyglot, extensible source code indexer.',
+    license = 'MIT',
+    keywords = 'source indexing',
+    url = 'http://github.com/abingham/stag',
+
+#    entry_points = {
+#        'console_scripts': [
+#            '{} = traad.server:main'.format(script_name),
+#            ],
+#        },
+
+    install_requires=[
+        'baker',
+        'pykka',
+    ],
+)
