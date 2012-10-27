ed10affbcfa0ce662267ace2d123bf23677301a4
Austin Bingham <austin.bingham@gmail.com>
2012-10-27 22:56:05 +0200

    pylint-ed clang_parser

--- stag/parser/clang_parser.py
+++ stag/parser/clang_parser.py
@@ -1,3 +1,8 @@
+"""Provides a clang-based C/C++ parser.
+"""
+
+import subprocess
+
 class Parser:
     """A parser for C/C++ code which uses an external clang-based
     tool.
@@ -11,18 +16,30 @@ class Parser:
 
     @property
     def fname(self):
+        """The filename being parsed."""
         return self._fname
 
     def set_file(self, fname):
+        """Set the filename to be parsed.
+
+        Args:
+          fname: A file name.
+
+        """
+
         self._fname = fname
 
     def definitions(self):
+        """Generate the definitions in ``self.fname``.
+
+        """
+
         assert self._fname is not None
 
         result = subprocess.check_output(
             ["echo", "someFunction 42"],
             universal_newline=True)
 
-        for r in result.split('\n'):
-            name, lineno = r.split()
+        for refn in result.split('\n'):
+            name, lineno = refn.split()
             yield name, int(lineno)
